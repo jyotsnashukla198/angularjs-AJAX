2e36fddba901d7dcaeba8f749d1eff401cd36c87
jyotsnashukla198 <jyotsnashukla198@gmail.com>
2015-03-21 15:46:16 +0530

    Update

--- testing.js
+++ testing.js
@@ -1,11 +1,12 @@
 <script src="/assets/modernizr-tests-ccd0969e74790fbc9b3b7aaf586aeb1a.js"></script>
   <!--[if (gte IE 6)&(lte IE 8)]>
   <script src="/javascripts/selectivizr-min.js"></script>
-  <![endif]-->
+  <![endif]-------->
 
   <script src="https://use.typekit.com/jsq2fql.js" type="text/javascript"></script>
   <script>
-    //<![CDATA[
+    
       try{Typekit.load();}catch(e){}
     //]]>
   </script>
+add
